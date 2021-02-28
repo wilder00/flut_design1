2965407b5751772f3cf47ab093bc9452e410a8ca
Wilder Trujillo <nadie437@gmail.com>
2021-02-28 11:38:08 -0500

    139. Diseño Compuesto - Blur y argumentos

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,7 @@ class BotonesPage extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 _titulos(),
+                _botonesRedondeados(),
               ],
             ),
           ),
@@ -122,6 +124,73 @@ class BotonesPage extends StatelessWidget {
             title: Container(),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _botonesRedondeados() {
+    return Table(
+      children: <TableRow>[
+        TableRow(
+          children: [
+            _crearBotonRedondeado(Colors.blue, Icons.border_all, 'General'),
+            _crearBotonRedondeado(
+                Colors.purpleAccent, Icons.directions_bus, "Bus"),
+          ],
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
+            _crearBotonRedondeado(
+                Colors.orange, Icons.insert_drive_file, 'File'),
+          ],
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(
+                Colors.blueAccent, Icons.movie_filter, 'Entertaiment'),
+            _crearBotonRedondeado(Colors.green, Icons.cloud, 'Grocery'),
+          ],
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(Colors.red, Icons.collections, 'Photos'),
+            _crearBotonRedondeado(Colors.teal, Icons.help_outline, 'General'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _crearBotonRedondeado(Color color, IconData icono, String texto) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5.05, sigmaY: 15.05),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(62, 66, 107, 0.7),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(
+                  icono,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                texto,
+                style: TextStyle(color: Colors.pinkAccent),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
