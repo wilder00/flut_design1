e3cbc61b0a0367627730ab556df7dbae79b9fc84
Wilder Trujillo <nadie437@gmail.com>
2021-02-27 21:27:28 -0500

    Scroll design 2

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -15,10 +15,60 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Pagina1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
   }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image(
+            image: AssetImage("assets/img/scroll-1.png"), fit: BoxFit.cover));
+  }
+
+  Widget _textos() {
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            '11°',
+            style: estiloTexto,
+          ),
+          Text(
+            'Sábado',
+            style: estiloTexto,
+          ),
+          Expanded(child: Container()),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 70.0,
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+
+// Comienza Pagina 2
 
   Widget _pagina2() {
     return Center(
