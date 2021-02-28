fabaee7c8ebc03073c88b15c24ed9746e8c568b6
Wilder Trujillo <nadie437@gmail.com>
2021-02-27 20:23:06 -0500

    diseño básico parte 1

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold);
+  final estiloSubTitulo = TextStyle(fontSize: 15.0, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Basico Page"),
-      ),
+      body: Column(children: <Widget>[
+        Image(
+          image: NetworkImage(
+              "https://tokens.gala.games/images/sandbox-games/mirandus/screenshots/medium/Mirandus_Scene.jpg"),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Lago en un puente",
+                      style: estiloTitulo,
+                    ),
+                    SizedBox(
+                      height: 7.0,
+                    ),
+                    Text(
+                      "Un lago en Alemania",
+                      style: estiloSubTitulo,
+                    ),
+                  ],
+                ),
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.red,
+                size: 30.0,
+              ),
+              Text(
+                '41',
+                style: TextStyle(fontSize: 18.0),
+              )
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
