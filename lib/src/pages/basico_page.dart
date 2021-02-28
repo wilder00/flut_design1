0f1e9f5406576fcd78cf09a2e773be50af32f6ed
Wilder Trujillo <nadie437@gmail.com>
2021-02-27 20:58:22 -0500

    Diseño básico parte 3, usando el SingleChildScrollView para poder ver un column con scroll

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -7,58 +7,74 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
-        _crearImagen(),
-        _crearTitulo(),
-        _crearAcciones(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-      ]),
+      //permise hacer scroll de un único hijo que en este caso es una columna (mantiene es notch)
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ],
+        ),
+      ),
     );
   }
 
   Widget _crearImagen() {
-    return Image(
-      image: NetworkImage(
-          "https://tokens.gala.games/images/sandbox-games/mirandus/screenshots/medium/Mirandus_Scene.jpg"),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            "https://tokens.gala.games/images/sandbox-games/mirandus/screenshots/medium/Mirandus_Scene.jpg"),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Lago en un puente",
-                  style: estiloTitulo,
-                ),
-                SizedBox(
-                  height: 7.0,
-                ),
-                Text(
-                  "Un lago en Alemania",
-                  style: estiloSubTitulo,
-                ),
-              ],
+    //con estoe vitamos que al poner en modo horizontal, el textos e vea tapado por el notch
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Lago en un puente",
+                    style: estiloTitulo,
+                  ),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text(
+                    "Un lago en Alemania",
+                    style: estiloSubTitulo,
+                  ),
+                ],
+              ),
             ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red,
-            size: 30.0,
-          ),
-          Text(
-            '41',
-            style: TextStyle(fontSize: 18.0),
-          )
-        ],
+            Icon(
+              Icons.star,
+              color: Colors.red,
+              size: 30.0,
+            ),
+            Text(
+              '41',
+              style: TextStyle(fontSize: 18.0),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -94,11 +110,15 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTexto() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        "Dolor irure exercitation voluptate ullamco magna minim mollit amet excepteur eiusmod occaecat duis. Aute anim velit pariatur adipisicing anim reprehenderit nisi et veniam laborum duis id et irure. Adipisicing sint id sint ad do pariatur deserunt ad tempor pariatur fugiat occaecat.",
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 40.0, /*  vertical: 20.0 */
+        ),
+        child: Text(
+          "Dolor irure exercitation voluptate ullamco magna minim mollit amet excepteur eiusmod occaecat duis. Aute anim velit pariatur adipisicing anim reprehenderit nisi et veniam laborum duis id et irure. Adipisicing sint id sint ad do pariatur deserunt ad tempor pariatur fugiat occaecat.",
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
